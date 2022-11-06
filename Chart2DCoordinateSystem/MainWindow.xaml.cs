90ab88adad0cd6b3051057c3178f16837dd95baf
gideon <edoghotugiddy@gmail.com>
2022-11-07 00:16:53 +0100

    edited

--- Chart2DCoordinateSystem/MainWindow.xaml.cs
+++ Chart2DCoordinateSystem/MainWindow.xaml.cs
@@ -19,14 +19,14 @@ namespace Chart2DCoordinateSystem
         private void AddGraphics()
         {
             line1 = new Line();
-            line1.X1 = XNormalize(3.0);
+            line1.X1 = XNormalize(2.0);
             line1.Y1 = YNormalize(4.0);
-            line1.X2 = XNormalize(9.0);
+            line1.X2 = XNormalize(8.0);
             line1.Y2 = YNormalize(10.0);
 
             line1.Stroke = Brushes.Blue;
             line1.StrokeThickness = 2;
-            plotCanvas.Children.Add(line1);
+            chartCanvas.Children.Add(line1);
 
             polyline1 = new Polyline();
             polyline1.Points.Add(new Point(XNormalize(8), YNormalize(8)));
@@ -35,20 +35,29 @@ namespace Chart2DCoordinateSystem
             polyline1.Points.Add(new Point(XNormalize(4), YNormalize(4)));
             polyline1.Points.Add(new Point(XNormalize(4), YNormalize(6)));
             polyline1.Points.Add(new Point(XNormalize(6), YNormalize(6)));
+
+            //polyline1.Points.Add(new Point(XNormalize(8), YNormalize(8)));
+            //polyline1.Points.Add(new Point(XNormalize(6), YNormalize(6)));
+            //polyline1.Points.Add(new Point(XNormalize(6), YNormalize(4)));
+            //polyline1.Points.Add(new Point(XNormalize(5), YNormalize(2)));
+            //polyline1.Points.Add(new Point(XNormalize(4), YNormalize(4)));
+            //polyline1.Points.Add(new Point(XNormalize(4), YNormalize(6)));
+            //polyline1.Points.Add(new Point(XNormalize(5), YNormalize(7)));
+            //polyline1.Points.Add(new Point(XNormalize(6), YNormalize(6)));
             polyline1.Stroke = Brushes.Red;
             polyline1.StrokeThickness = 5;
-            plotCanvas.Children.Add(polyline1);
+            chartCanvas.Children.Add(polyline1);
         }
 
         private double YNormalize(double y)
         {
-            double result = plotCanvas.Height - (y - yMin) * plotCanvas.Height / (yMax - yMin);
+            double result = chartCanvas.Height - (y - yMin) * chartCanvas.Height / (yMax - yMin);
             return result;
         }
 
         private double XNormalize(double x)
         {
-            double result = (x - xMin) * plotCanvas.Width / (xMax - xMin);
+            double result = (x - xMin) * chartCanvas.Width / (xMax - xMin);
             return result;
         }
 
@@ -58,8 +67,8 @@ namespace Chart2DCoordinateSystem
             xMax = Convert.ToDouble(tbXMax.Text);
             yMin = Convert.ToDouble(tbXMin.Text);
             yMax = Convert.ToDouble(tbYMax.Text);
-            plotCanvas.Children.Remove(line1);
-            plotCanvas.Children.Remove(polyline1);
+            chartCanvas.Children.Remove(line1);
+            chartCanvas.Children.Remove(polyline1);
             AddGraphics();
         }
 
